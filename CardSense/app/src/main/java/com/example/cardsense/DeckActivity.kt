e12bdd1b6acd7eec44c4c83ccbd2c91989f0bd16
Jorge Diaz <90347792+Diaz-jorge939@users.noreply.github.com>
2023-06-25 15:17:12 -0700

    created new activities and layouts

---
+++ CardSense/app/src/main/java/com/example/cardsense/DeckActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cardsense
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deck)
+    }
+}
